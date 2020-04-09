be3d2d555b80cf2649a33414e94188f6f6b7206a
Thomas John Helin <thomashelin@yahoo.com>
2020-04-09 18:11:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,6 +27,7 @@ def monopoly_with_third_tier
 	monopoly[:railroads] = {}
 	monopoly[:railroads][:pieces] = 4
   monopoly[:railroads][:names] = {}
+  monopoly[:railroads][:rent_in_dollars] = {}
   monopoly
 end
 
